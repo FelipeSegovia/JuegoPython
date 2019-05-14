1114b9001cb729a09d185be9d85752b4400ec180
Norton <Norton.Dante.I@gmail.com>
2019-05-14 03:18:48 -0600

    Debemos usar dos archivos
    Debemos usar dos archivos uno es el original  y el otro es el que van a ir modificando cuando esten jugando mirar codigo en los comentarios.

--- cliente.py
+++ cliente.py
@@ -1,6 +1,22 @@
 import xmlrpc.client
 
-with xmlrpc.client.ServerProxy("http://192.168.0.16:8000/") as proxy:
-    lista = proxy.mostrar_mapa()
-    for i in range(len(lista)):
-        print(lista[i])
+while True:
+    with xmlrpc.client.ServerProxy("http://192.168.0.2:8000/") as proxy:
+        print("Bienvenido al pilla pilla")
+        print("0 para salir")
+        print("1 para mostrar mapa")
+        print("2 para jugar")
+        op = input("Ingrese opcion:")
+        
+        if (op=="1"):
+            matriz = (proxy.mostrar_mapa())
+            for i in range(len(matriz)):             
+                print(matriz[i])
+        elif (op=="2"):
+            id = input("Ingrese la inicial de su nombre:")
+            matriz = (proxy.colocar_jugador(id))
+        elif (op=="0"):
+            break
+        
+        
+        
--- servidor.py
+++ servidor.py
@@ -1,14 +1,34 @@
 from xmlrpc.server import SimpleXMLRPCServer
+server = SimpleXMLRPCServer(("192.168.0.2", 8000))
 
+matriz = []
+archivo = open('mapa.txt','r') # este es el mapa inicial
+for linea in archivo:
+    matriz.append(linea.strip("\r\n"))
+archivo.close()
+
+def resetear_mapa():
+    archivo = open ('mapa_m.txt','a') # reseteo el mapa cuando todos los jugadores salen de la partida o esta termina
+    #for i in range(len(matriz)):
+    #    archivo.write(matriz[i])
+    #archivo.close()
+    #return 1;
+    
 def mostrar_mapa():
-    a = []
-    archivo = open('mapa.txt','r')
+    matriz_m = []
+    archivo = open('mapa_m.txt','r') # este es el mapa que se va a ir modificando
     for linea in archivo:
-        a.append(linea.strip("\r\n"))
-    archivo.close
-    return a
+        matriz_m.append(linea.strip("\r\n"))
+    archivo.close()
+    return (matriz_m)
 
-server = SimpleXMLRPCServer(("192.168.0.16", 8000))
-print("Listening on port 8000...")
+
+def colocar_jugador(id):
+    return 1
+
+
+print("Esperando al cliente.. puerto 8000")
+server.register_function(resetear_mapa, "resetear_mapa")
 server.register_function(mostrar_mapa, "mostrar_mapa")
+server.register_function(colocar_jugador,"colocar_jugador")
 server.serve_forever()
