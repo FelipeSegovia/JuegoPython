1f5d932eb9bb64ac7b765f96b1178be2b581dc6c
PipeSegovia <fsegoviar@alumnosuls.cl>
2019-05-16 00:54:29 -0400

    Se logra generar una conversacion entre dos clientes

--- cliente.py
+++ cliente.py
@@ -2,7 +2,18 @@ import socket
 import os
 import subprocess
 import time
+import threading
 
+apagar = False
+def recibir(nombre,socket):
+    while not apagar:
+        try:
+            while True:
+                data = socket.recv(1024)
+                print(str(data))
+        except:
+            pass
+            
 def Main():
     
     host = "192.168.1.20"
@@ -10,17 +21,41 @@ def Main():
 
     socket_cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     socket_cliente.connect((host,puerto))
-    """
+
+    for i in range(5):
+        threading.Thread(target=recibir,args=("Hilo",socket_cliente)).start()
+    
     alias = input("Ingrese su nombre: ")
 
-    mensaje = input(alias+" ->")
+    mensaje = input(alias+ ": ")
 
-    while mensaje != "q":
-        socket_cliente.sendto("{}->{}".format(alias,mensaje).encode("utf-8"),(host,puerto))
-        mensaje = socket_cliente.recv(1024)
-        print(mensaje.decode('ascii')) 
+    while mensaje != 'q':
+        if mensaje != '':
+            socket_cliente.sendto("{}->{}".format(alias,mensaje).encode("utf-8"),(host,puerto))
+
+        mensaje = input(alias+ ": ")
+        time.sleep(0.1)
+    
+    socket_cliente.close()
     """
+    mensaje_servidor = socket_cliente.recv(1024)
+    mensaje_servidor = mensaje_servidor.decode("utf-8")
+    print(str(mensaje_servidor))
 
+    alias = input("Ingrese su nombre: ")
+
+    mensaje = ''
+
+    while True:
+        if mensaje !='':
+            socket_cliente.sendto("{}->{}".format(alias,mensaje).encode("utf-8"),(host,puerto))
+        mensaje = input(alias+"->")
+        data = socket_cliente.recv(1024)
+        data = data.decode("utf-8")
+        print(str(data))
+    """
+    
+    """
     while True:
         data = socket_cliente.recv(1024)
         if data[:2].decode("utf-8") == 'cd':
@@ -32,7 +67,7 @@ def Main():
             currentWD = os.getcwd() + "> "
             socket_cliente.send(str.encode(output_str + currentWD))
             print(output_str)
-
+    """
 
 if __name__ == "__main__":
     Main()
--- servidor.py
+++ servidor.py
@@ -12,32 +12,13 @@ cola = Queue()
 conexiones = []
 direcciones = []
 
-def crear_socket():
-    try:
-        global host
-        global puerto
-        global socket_servidor
+host = "192.168.1.20"
+puerto = 8000
+socket_servidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-        host  = "192.168.1.20"
-        puerto = 8000
-        socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as msg:
-        print("Error al crear socket: "+ str(msg))
-
-def conectar_socket():
-    try:
-        global host
-        global puerto
-        global socket_servidor
-        print("Socket conectado: "+ str(puerto))
-
-        socket_servidor.bind((host,puerto))
-        socket_servidor.listen(5) #acepta 5 conexiones 
-    except socket.error as msg:
-        print("Error al conectar el socket: "+ str(msg)+"\nReinentando...")
-        conectar_socket()
-
-def aceptar_conexiones():
+socket_servidor.bind((host,puerto))
+socket_servidor.listen(5)
+def iniciar():
     for c in conexiones:
         c.close()
 
@@ -55,49 +36,31 @@ def aceptar_conexiones():
 
             print("({})Conexion desde {} a sido establecida!".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()),dir[0]))
             #Envio datos del servidor al cliente
-            #con.send(bytes("Bienvenido al servidor!","utf-8"))
+            con.send(bytes("Bienvenido al servidor!","utf-8"))
 
             #Recibo datos desde el cliente al servidor
-            #dato = con.recv(1024)
-            #print('Recibi: {!r}'.format(dato))
-            #con.sendall(dato)
+            dato = con.recv(1024)
+            print(dir[0])
+            print('Recibi: {!r}'.format(dato))
+            for direccion in direcciones:
+                if direccion != dir:
+                    con.sendall(dato)
             
             
         except:
             print("Error en la conexion")
+    
 
-def crear_hilos():
+def Main():
+
     for _ in range(NUMERO_DE_HILOS):
         hilo = threading.Thread(target=iniciar)
         hilo.daemon = True
         hilo.start()
-
-
-def iniciar():
-    while True:
-        x = cola.get()
-        if x == 1:
-            crear_socket()
-            conectar_socket()
-            aceptar_conexiones()
-        
-        cola.task_done()
-
-def crear_trabajos():
+    
     for x in NUMERO_JOBS:
         cola.put(x)
     cola.join()
-
-def Main():
-
-    print("Hola desde el servidor")
-    crear_hilos()
-    crear_trabajos()
-
-        
-    
-        
-
 
 if __name__ == "__main__":
     Main()
